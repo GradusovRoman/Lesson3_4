6335a8b0c9871778dc97bf3c2292c09c3f2ce404
GradusovRoman <monomari86@gmail.com>
2020-12-02 18:34:01 +0500

    Lesson3_4 try #1

---
+++ src/geekbrains/lesson3_4gb/PrintABC.java
@@ -0,0 +1,39 @@
+package geekbrains.lesson3_4gb;
+
+public class PrintABC {
+    static volatile char c = 'A';
+    static Object monitor = new Object();
+
+    static class doOrderABC implements Runnable {
+        private char currentLetter;
+        private char nextLetter;
+
+        public doOrderABC (char currentLetter, char nextLetter) {
+            this.currentLetter = currentLetter;
+            this.nextLetter = nextLetter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                synchronized (monitor) {
+                    try {
+                        while (c != currentLetter)
+                            monitor.wait();
+                        System.out.print(currentLetter);
+                        c = nextLetter;
+                        monitor.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        new Thread(new doOrderABC('A', 'B')).start();
+        new Thread(new doOrderABC('B', 'C')).start();
+        new Thread(new doOrderABC('C', 'A')).start();
+    }
+}
